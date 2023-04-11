f4a49a3cd224dcc1506a7cfe9e8dc4107e97ed06
Renato Aloi <renato.aloi@gmail.com>
2023-04-11 08:17:38 -0300

    Update OkGoogleEsp01.ino

--- OkGoogleEsp01.ino
+++ OkGoogleEsp01.ino
@@ -19,6 +19,15 @@
 #define AIO_KEY       "chave"   // Replace with your Project Auth Key
 
 /************ Global State (you don't need to change this!) ******************/
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define Relay1            0
+
+#define AIO_FEED_NAME1 "/feeds/quarto" // replace with your feed
+
+/************ Global State (you don't need to change this!) ******************/
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
@@ -32,12 +41,7 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 
 
 // Setup a feed called 'onoff' for subscribing to changes.
-Adafruit_MQTT_Subscribe Light1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/luz"); // FeedName
-//Adafruit_MQTT_Subscribe Light2 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME, "/feeds/Relay2");
-//Adafruit_MQTT_Subscribe Light3 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME, "/feeds/Relay3");
-//Adafruit_MQTT_Subscribe Light4 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME, "/feeds/Relay4");
-
-
+Adafruit_MQTT_Subscribe Light1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME AIO_FEED_NAME1);
 
 void MQTT_connect();
 
@@ -45,9 +49,6 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(Relay1, OUTPUT);
-//  pinMode(Relay2, OUTPUT);
-//  pinMode(Relay3, OUTPUT);
-//  pinMode(Relay4, OUTPUT);
   
   // Connect to WiFi access point.
   Serial.println(); Serial.println();
@@ -68,16 +69,12 @@ void setup() {
 
   // Setup MQTT subscription for onoff feed.
   mqtt.subscribe(&Light1);
-//  mqtt.subscribe(&Light3);
-//  mqtt.subscribe(&Light2);
-//  mqtt.subscribe(&Light4);
 }
 
 void loop() {
  
   MQTT_connect();
   
-
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(20000))) {
     if (subscription == &Light1) {
@@ -85,30 +82,8 @@ void loop() {
       Serial.println((char *)Light1.lastread);
       int Light1_State = atoi((char *)Light1.lastread);
       digitalWrite(Relay1, !Light1_State);
-      
     }
-//    if (subscription == &Light2) {
-//      Serial.print(F("Got: "));
-//      Serial.println((char *)Light2.lastread);
-//      int Light2_State = atoi((char *)Light2.lastread);
-//      digitalWrite(Relay2, Light2_State);
-//    }
-//    if (subscription == &Light3) {
-//      Serial.print(F("Got: "));
-//      Serial.println((char *)Light3.lastread);
-//      int Light3_State = atoi((char *)Light3.lastread);
-//      digitalWrite(Relay3, Light3_State);
-//    }
-//    if (subscription == &Light4) {
-//      Serial.print(F("Got: "));
-//      Serial.println((char *)Light4.lastread);
-//      int Light4_State = atoi((char *)Light4.lastread);
-//      digitalWrite(Relay4, Light4_State);
-//      
-//    }
   }
-
-  
 }
 
 void MQTT_connect() {
@@ -135,5 +110,4 @@ void MQTT_connect() {
     }
   }
   Serial.println("MQTT Connected!");
-  
 }
